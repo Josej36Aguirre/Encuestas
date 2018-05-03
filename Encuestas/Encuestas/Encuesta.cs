0cced7b0d0481a208350f1429e77ff535ce0ef59
Josej36Aguirre <josej36.aguirre@gmail.com>
2018-05-02 21:37:20 -0500

    resibimos un mensaje y enviamos un mensaje

---
+++ Encuestas/Encuestas/Encuesta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encuestas
+{
+    public class Encuesta
+    {
+        #region Propiedades
+        public String Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public String EquipoFavorito { get; set; }
+        #endregion
+        #region Metodo
+        public override string ToString()
+        {
+            return $"{Nombre}|{FechaNacimiento}|{EquipoFavorito}";
+        }
+
+        
+        #endregion
+    }
+}
--- Encuestas/Encuestas/EncuestasView.xaml.cs
+++ Encuestas/Encuestas/EncuestasView.xaml.cs
@@ -17,7 +17,15 @@ namespace Encuestas
 			InitializeComponent ();
 
             btnMas.Clicked += BtnMas_Clicked;
-        }
+            //aqui se crea un metod anonimo y se crea con lamda 
+            MessagingCenter.Subscribe<ContentPage, Encuesta>(this, Mensajes.EncuestaCompleta, (sender, args) 
+                => {
+                    Panel.Children.Add(new Label()
+                    {
+                        Text = args.ToString()
+                    });
+                });
+         }
 
         private async void BtnMas_Clicked(object sender, EventArgs e)
         {
