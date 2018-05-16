9cde1cbb430c69045087e9652f95ea4e64ddf2b7
Josej36Aguirre <josej36.aguirre@gmail.com>
2018-05-15 21:49:36 -0500

    final

--- Encuestas/Encuestas/EncuestasView.xaml.cs
+++ Encuestas/Encuestas/EncuestasView.xaml.cs
@@ -18,13 +18,7 @@ namespace Encuestas
 
             btnMas.Clicked += BtnMas_Clicked;
             //aqui se crea un metod anonimo y se crea con lamda 
-            MessagingCenter.Subscribe<ContentPage, Encuesta>(this, Mensajes.EncuestaCompleta, (sender, args) 
-                => {
-                    Panel.Children.Add(new Label()
-                    {
-                        Text = args.ToString()
-                    });
-                });
+           
          }
 
         private async void BtnMas_Clicked(object sender, EventArgs e)
